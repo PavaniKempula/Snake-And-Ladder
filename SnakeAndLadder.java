b0be6df61509fbd794d9eb6c61d3adb6b86c499e
PavaniKempula <pavanikempula1998@gmail.com>
2021-07-29 19:11:45 +0530

    ADDED : Welcome to Snake and Ladder Game

---
+++ SnakeAndLadder.java
@@ -0,0 +1,11 @@
+package com.bridgeladz;
+
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+
+		// Displaying Welcome Message
+		System.out.println("Welcome To Snake And Ladder Game");
+	}
+
+}
