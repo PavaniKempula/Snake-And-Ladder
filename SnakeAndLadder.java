4edae636cb91588dec4972e87ed4ccf9f24c0a09
PavaniKempula <pavanikempula1998@gmail.com>
2021-07-29 19:42:44 +0530

    MODIFIED : Initializing Starting Position and Displaying it

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,10 +2,16 @@ package com.bridgeladz;
 
 public class SnakeAndLadder {
 
+	final static int NO_OF_PLAYER = 1;
+
 	public static void main(String[] args) {
 
 		// Displaying Welcome Message
 		System.out.println("Welcome To Snake And Ladder Game");
+		int position = 0;
+		System.out.println("Player start position : " + position);
+		System.out.println("Number of player is:" + NO_OF_PLAYER);
+
 	}
 
 }
