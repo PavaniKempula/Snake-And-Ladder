366a1fac37b9718c12b4dc0973c16004f0543dfc
PavaniKempula <pavanikempula1998@gmail.com>
2021-07-30 07:28:40 +0530

    ADDED : Taking a note of how many times dice has been rolled and at the end of the program printing the count

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -18,9 +18,11 @@ public class SnakeAndLadder {
 		int reach = 0;
 		int roll = 0;
 		int start = 0;
+		int counter = 0;
 
 		while (reach < WINNING_POSITION) {
 			reach++;
+			
 			/*
 			 * Using random to get Dice number
 			 * Math.random() function generating random numbers from range 1 to 6
@@ -33,25 +35,30 @@ public class SnakeAndLadder {
 			int option = (int) Math.floor(Math.random() * 10) % 3;
 			System.out.println("Option is :" + option);
 			
-			//Repeating till the Player reaches the Exact Winning Position
 			switch (option) {
 			case NO_PLAY:
 				roll = 0;
 				break;
 			case LADDER:
+				counter++;
 				roll = +diceValue;
 				break;
 			case SNAKE:
 				roll = -diceValue;
 				break;
 			}
-
+			
+			/*
+			 * Repeating till the Player reaches the winning position 100
+			 * Reporting the number of times the dice was played to win 
+			 * Also Showing Current Position after every dice roll
+			 */
 			position = (position + roll);
 			if ((position < start) && (option == 0)) {
 				System.out.println("Current Position: " + start);
 				reach = start;
 				System.out.println("Reached: " + reach);
-			} else if ((position > WINNING_POSITION) && (option == 1)) {
+			} else if ((position > WINNING_POSITION) && ((option == 1) || (option == 2))) {
 				System.out.println("Current Position: " + position);
 				reach = WINNING_POSITION;
 				System.out.println("Reached: " + reach);
@@ -66,6 +73,7 @@ public class SnakeAndLadder {
 				System.out.println("Reached: " + reach);
 			}
 		}
+		System.out.println("Number of time dice was rolled to win game :" + counter);
 		System.out.println("Win:" + reach);
 	}
 }
